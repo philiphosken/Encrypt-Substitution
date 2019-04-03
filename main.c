861ae864f3b8446d1b0e5fd4652821f71214e6e0
Philip Hosken <philip.hosken@uon.edu.au>
2019-04-03 13:26:03 +0000

    Quick Brainstrorm for how to do sub without physicsally saying this ascii will now be this ascii etc

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
 #include <stdio.h>
+
+int main()
+{
+char alphatxt[] = "TEST"; //Text that is to become encrypted
+int N = 4; //Number of Values in the Array above
+int i; //Counter Index
+
+//could do all ascii that is div by two shift 4 right all div by 3 move left 2 etc 
+
+}
